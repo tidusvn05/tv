8d81bcd04334b29b26170dd58771c0bb3775c6a8
D.Nobita <tidusvn05@gmail.com>
2024-01-14 09:07:29 +0700

    auto update

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 /Users/tidusvn05/.nvm/versions/node/v16.20.0/bin/node sync_iptv7.js
-git commit -am "up" && git push origin main
+git commit -am "auto update" && git push origin main
 
 echo "ran at $(date)" >> running.log
